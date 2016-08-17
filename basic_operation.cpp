9263ae0ad93752038bd546e6418b42d6e5ec4805
Yoonku Hwang <yz0624@gmail.com>
2016-08-17 17:39:16 +0900

    binary operations

--- basic_operation.cpp
+++ basic_operation.cpp
@@ -26,5 +26,34 @@ int main()
 
     std::cout << "add \t" << m1 + m2 << std::endl;
     std::cout << "subt \t" << m1 - m2 << std::endl;
+
+    // scalar * Matrix
+    matrix<double> mm (3, 3);
+    for (unsigned i = 0; i < mm.size1 (); ++ i)
+        for (unsigned j = 0; j < mm.size2 (); ++ j)
+            mm (i, j) = 3 * i + j;
+
+    std::cout << "2 * matrix \t" << 2.0 * mm << std::endl;
+    std::cout << "matrix * 2 \t" << mm * 2.0 << std::endl;
+
+    // Matrix * Vector
+    matrix<double> m_ (3, 3);
+    vector<double> v (3);
+    for (unsigned i = 0; i < std::min (m_.size1 (), v.size ()); ++ i) {
+        for (unsigned j = 0; j < m_.size2 (); ++ j)
+            m_ (i, j) = 3 * i + j;
+        v (i) = i;
+    }
+
+    std::cout << "matrix * vector (A * b) \t" << prod (m_, v) << std::endl;
+    std::cout << "vector * matrix (A' * b)\t" << prod (v, m_) << std::endl;   
+
+    // Matrix * Matrix
+    matrix<double> m11 (3, 3), m22 (3, 3);
+    for (unsigned i = 0; i < std::min (m11.size1 (), m22.size1 ()); ++ i)
+        for (unsigned j = 0; j < std::min (m11.size2 (), m22.size2 ()); ++ j)
+            m11 (i, j) = m22 (i, j) = 3 * i + j;
+
+    std::cout << "matrix * matrix (A * B) \t" << prod (m11, m22) << std::endl;
 	return 0;
 }
