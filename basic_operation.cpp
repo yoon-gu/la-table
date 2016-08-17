550709d0b0e6926f423df7933ad5a12085186f4a
Yoonku Hwang <yz0624@gmail.com>
2016-08-17 17:31:44 +0900

    add binary operation(+/-)

--- basic_operation.cpp
+++ basic_operation.cpp
@@ -5,6 +5,7 @@ using namespace boost::numeric::ublas;
 
 int main()
 {
+    // op(Matrix)
 	matrix<std::complex<double> > m (3, 3);
     for (unsigned i = 0; i < m.size1 (); ++ i)
         for (unsigned j = 0; j < m.size2 (); ++ j)
@@ -17,5 +18,13 @@ int main()
     std::cout << "trans \t" << trans (m) << std::endl;
     std::cout << "herm \t" << herm (m) << std::endl;
 
+    // Matrix +/- Matrix
+    matrix<double> m1 (3, 3), m2 (3, 3);
+    for (unsigned i = 0; i < std::min (m1.size1 (), m2.size1 ()); ++ i)
+        for (unsigned j = 0; j < std::min (m1.size2 (), m2.size2 ()); ++ j)
+            m1 (i, j) = m2 (i, j) = 3 * i + j;
+
+    std::cout << "add \t" << m1 + m2 << std::endl;
+    std::cout << "subt \t" << m1 - m2 << std::endl;
 	return 0;
 }
